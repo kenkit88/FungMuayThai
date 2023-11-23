be51e613657b63416bf22c16e8e8732650aaaa91
kenkit88 <kenkit888@gmail.com>
2023-11-23 18:16:22 +0800

    init

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/*$(function(){
+    $("#HeaderHtml").load("header.html");
+    $("#FooterHtml").load("footer.html");
+
+    $(function() {  
+        $(".nice-scroll").niceScroll({
+            cursorwidth: "5px",
+            background: "#e5e5e5",
+            cursorborder: "",
+            autohidemode: "leave",
+            hidecursordelay: 200,
+            horizrailenabled: false
+        });
+    });
+});*/
+
